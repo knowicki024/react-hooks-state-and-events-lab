cbdb69a995dc41f127d8464670e2dd67eed954a5
Kaitlyn Nowicki <katie.nowicki024@gmail.com>
2023-12-13 13:20:43 -0500

    pair lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const handleToggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+
+  const appClass = darkMode ? "App dark" : "App light";
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleToggleDarkMode}>{darkMode? "Light Mode" : "Dark Mode"} </button>
       </header>
       <ShoppingList items={itemData} />
     </div>
@@ -20,3 +28,4 @@ function App() {
 }
 
 export default App;
+
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,30 @@
 import React from "react";
 import Item from "./Item";
+import { useState } from "react";
+
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  const handleFilterChange = (e) => {
+    setSelectedCategory(e.target.value);
+  };
+
+  // Function to filter items based on the selected category
+  const getFilteredItems = () => {
+    if (selectedCategory === "All") {
+      return items; // If "All" is selected, display all items
+    } else {
+      return items.filter((item) => item.category === selectedCategory);
+    }
+  };
+
+  const filteredItems = getFilteredItems();
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleFilterChange} value={selectedCategory}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +32,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
@@ -22,3 +41,4 @@ function ShoppingList({ items }) {
 }
 
 export default ShoppingList;
+
